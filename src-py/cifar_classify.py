169a9ef5cd3213bede36d2a4a18581190151b1dc
Abdelrahman Elbehery <abdo.m7med.89@gmail.com>
2017-09-27 23:25:35 +0200

    Add self.accuracy method @ KNN class Add debug option to self.classify instead of self.show_from_test

--- src-py/cifar_classify.py
+++ src-py/cifar_classify.py
@@ -45,13 +45,10 @@ class KNN(object):
         args :
          test_id : the index of the test item to view
         """
-        if debug:
-            print "showing test image number", test_id,\
-            "Which is a ", self.classes[self.y_test[test_id]]
         plt.imshow(self.x_test[test_id].reshape((32, 32, 3)).astype('uint8'))
         plt.show()
 
-    def classify(self, test_id, view=False):
+    def classify(self, test_id, view=False, internal=False, debug=False):
         """
         Predict an image given its dataset index
         args:
@@ -66,7 +63,26 @@ class KNN(object):
         sub_arr = sub_arr.argsort()[:self.num_k]
         # increment the repreated classes by 1 in the score array
         np.add.at(score_arr, self.y_train[sub_arr], 1)
-        if view: 
+        if view:
             self.show_from_test(test_id)
+        if debug:
+            print "showing test image number", test_id,\
+            "Which is a ", self.classes[self.y_test[test_id]]
         # take the highest scrore
+        if internal:
+            return score_arr.argsort()[-1]
         return self.classes[score_arr.argsort()[-1]]
+    
+    def accuracy(self, view_console=False):
+        """
+        Returns the model accuracy in percentage
+        """
+        correct = 0.0
+        l = self.x_test.shape[0]
+        for i in range(l):
+            if(self.classify(i,internal=True) == self.y_test[i]):
+                correct += 1
+        ret = correct/l * 100
+        if view_console:
+            print ret
+        return ret
