eb1f0277d0b849144d86bcda8b333b9c1bb3dc24
Abdelrahman Elbehery <abdo.m7med.89@gmail.com>
2017-11-26 22:20:49 +0200

    Add enhanced cifar10 neural network Add final readme

--- src-py/enhancing_cifar10.py
+++ src-py/enhancing_cifar10.py
@@ -6,9 +6,9 @@ import os
 import keras
 import numpy as np
 from keras.datasets import cifar10
-from keras.layers import Activation, Dense, regularizers
+from keras.layers import Activation, Dense, regularizers, Dropout
 from keras.models import Sequential
-from keras.optimizers import SGD
+from keras.optimizers import SGD, adam
 import pylab
 import time
 
@@ -30,7 +30,7 @@ y_valid = y_test[:5000, :]
 x_test = x_test[5000:, :]
 y_test = y_test[5000:, :]
 
-def test_nn_initw(num_epochs=25, eta=1e-2, batch_size=64, hidden_neurons=100, ran_w=0.4):
+def test_nn_initw(num_epochs=25, eta=1e-2, batch_size=64, hidden_neurons=100, ran_w=0.4, reg_str=0.4):
     """
     Creates a neural network FF and attempts to plot the training/valid loss and acc
     with regularization
@@ -44,39 +44,46 @@ def test_nn_initw(num_epochs=25, eta=1e-2, batch_size=64, hidden_neurons=100, ra
     model = Sequential()
 
     # create an input layer followed by 50 sigmoid neurons
-    in_layer = Dense(hidden_neurons, input_dim=3072,activation='sigmoid',kernel_initializer=keras.initializers.RandomUniform(-ran_w, ran_w))
+    in_layer = Dense(hidden_neurons, input_dim=3072,activation='relu',kernel_initializer=keras.initializers.RandomUniform(-ran_w, ran_w),
+                     kernel_regularizer=regularizers.l2(reg_str))
     model.add(in_layer)
-
+    model.add(Dropout(0.4))
     # 2nd hidden layer of 50 sigmoid neurons
-    model.add(Dense(hidden_neurons,activation='sigmoid',kernel_initializer=keras.initializers.RandomUniform(-ran_w, ran_w)))
+    model.add(Dense(hidden_neurons,activation='relu',kernel_initializer=keras.initializers.RandomUniform(-ran_w, ran_w),
+                    kernel_regularizer=regularizers.l2(reg_str)))
+    model.add(Dropout(0.4))
     # output layer with 10 softmax neurons
-    model.add(Dense(10, activation='softmax',kernel_initializer=keras.initializers.RandomUniform(-ran_w, ran_w)))
+    model.add(Dense(10, activation='softmax',kernel_initializer=keras.initializers.RandomUniform(-ran_w, ran_w),
+                    kernel_regularizer=regularizers.l2(reg_str)))
 
     # optimizer object, with eta = 0.01
-    sgd = SGD(lr=eta)
-
+    #sgd = SGD(lr=eta)
+    sgd = SGD(lr=eta, decay=1e-4, momentum=0.9, nesterov=True)
     model.compile(loss="binary_crossentropy", optimizer=sgd, metrics=["categorical_accuracy"])
 
     t = time.time()
     # show the progress during iteration
-    history = model.fit(x_train, y_train, epochs=num_epochs, batch_size=batch_size, verbose=0, validation_data=(x_valid, y_valid))
+    history = model.fit(x_train, y_train, epochs=num_epochs, batch_size=batch_size, verbose=1, validation_data=(x_valid, y_valid))
     elapsed = time.time() - t
     print("Training time: ", elapsed, "S")
     print ("Train acc: ", history.history['categorical_accuracy'][-1])
     print ("Validation acc: ", history.history['val_categorical_accuracy'][-1])
-
-    pylab.plot(history.history['val_categorical_accuracy'])
-    pylab.plot(history.history['categorical_accuracy'])
-    pylab.legend(['Validation', 'Train'], loc='upper left')
-    pylab.title('Neural Network Accuracy')
-    pylab.xlabel('Epoch')
-    pylab.ylabel('Accuracy')
-    pylab.show()
-    pylab.plot(history.history['val_loss'])
-    pylab.plot(history.history['loss'])
-    pylab.legend(['Validation', 'Train'], loc='upper left')
-    pylab.title('Neural Network Loss')
-    pylab.xlabel('Epoch')
-    pylab.ylabel('Error')
-    pylab.show()
     return  model.evaluate(x_test, y_test,batch_size=batch_size, verbose=0)
+
+
+#_err, valid_acc = test_nn_initw(num_epochs = 22, ran_w=0.003, hidden_neurons=50, eta=1e-2, batch_size=64, reg_str=0.01)
+#print ("Error: ", _err, "\nTest Acc: ", valid_acc*100, "\n#####\n")
+
+# _err, test_acc = test_nn_initw(num_epochs = 22, ran_w=0.003, hidden_neurons=50, eta=1e-3, batch_size=64, reg_str=0.01) # 40%    #decay=1e-6, momentum=0.9
+# _err, test_acc = test_nn_initw(num_epochs = 32, ran_w=0.002, hidden_neurons=120, eta=1.5e-3, batch_size=80, reg_str=0.02) # 42% 
+
+# _err, test_acc = test_nn_initw(num_epochs = 32, ran_w=0.002, hidden_neurons=40, eta=2.5e-3, batch_size=128, reg_str=0*0.02) # 42.4%  # decay=1e-4, momentum=0.9
+# _err, test_acc = test_nn_initw(num_epochs = 32, ran_w=0.002, hidden_neurons=80, eta=2.5e-3, batch_size=128, reg_str=0*0.02) # 44.4% 
+# _err, test_acc = test_nn_initw(num_epochs = 32, ran_w=0.002, hidden_neurons=80, eta=2.5e-3, batch_size=256, reg_str=0*0.02) # 45.95% 
+# _err, test_acc = test_nn_initw(num_epochs = 32, ran_w=0.002, hidden_neurons=120, eta=2.5e-3, batch_size=256, reg_str=0*0.02) # 47.6% 
+# _err, test_acc = test_nn_initw(num_epochs = 32, ran_w=0.002, hidden_neurons=150, eta=2.5e-3, batch_size=256, reg_str=0*0.02) # 48.4% 
+# _err, test_acc = test_nn_initw(num_epochs = 80, ran_w=0.002, hidden_neurons=150, eta=2.5e-3, batch_size=512, reg_str=0*0.02) # 50.8% 
+# print ("Error: ", _err, "\nTest Acc: ", test_acc*100, "\n#####\n")
+
+_err, test_acc = test_nn_initw(num_epochs = 100, ran_w=0.002, hidden_neurons=150, eta=2.5e-3, batch_size=512, reg_str=0*0.02) # 50.8% 
+print ("Error: ", _err, "\nTest Acc: ", test_acc*100, "\n#####\n")
